a9455239b055489037d84523d3c9ad6d1b10aec2
lexiszafranski <aszafranski@ufl.edu>
2026-02-23 15:28:17 -0500

    switch over to browser vs hash

--- assessly/src/App.tsx
+++ assessly/src/App.tsx
@@ -1,17 +1,56 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import './styles/App.css';
 import Dashboard from './pages/dashboard';
 import Login from './pages/login';
 
 function App() {
   return (
-    <>
-      <SignedIn>
-        <Dashboard />
-      </SignedIn>
-      <SignedOut>
-        <Login />
-      </SignedOut>
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* Login Route */}
+        <Route
+          path="/login"
+          element={
+            <>
+              <SignedOut>
+                <Login />
+              </SignedOut>
+
+              {/* If already signed in, skip login */}
+              <SignedIn>
+                <Navigate to="/dashboard" replace />
+              </SignedIn>
+            </>
+          }
+        />
+
+        {/* Dashboard Route */}
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+
+              {/* If not signed in, go to login */}
+              <SignedOut>
+                <Navigate to="/login" replace />
+              </SignedOut>
+            </>
+          }
+        />
+
+        {/* Root login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Anything else = login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
