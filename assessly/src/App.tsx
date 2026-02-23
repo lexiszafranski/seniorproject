e928674af78dca8bae2287875b6b725f0da4affa
lexiszafranski <aszafranski@ufl.edu>
2026-02-23 14:54:00 -0500

    connected frontend and backend clerk

--- assessly/src/App.tsx
+++ assessly/src/App.tsx
@@ -1,23 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/dashboard'
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import Dashboard from './pages/dashboard';
 import Login from './pages/login';
-
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} /> 
-      </Routes>
-    </HashRouter>
-  )
+    <>
+      <SignedIn>
+        <Dashboard />
+      </SignedIn>
+      <SignedOut>
+        <Login />
+      </SignedOut>
+    </>
+  );
 }
-export default App
 
-// Testing 
-// http://localhost:<your local host num>/#/login
+export default App;
--- assessly/src/main.tsx
+++ assessly/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
+import './styles/index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const CLERK_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={CLERK_KEY}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
 )
