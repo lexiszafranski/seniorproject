2f3c339ade0043ade41a2bd196832963c47ffa02
Joyin Ang <joyinang2022@gmail.com>
2026-04-13 01:11:06 -0400

    fixed deployment unused variables

--- assessly/src/pages/tokens.tsx
+++ assessly/src/pages/tokens.tsx
@@ -1,6 +1,6 @@
 import loginImg from '../assets/Login/Login_Image.png';
 import '../styles/Login.css';
-import React, {useState} from 'react';
+import {useState} from 'react';
 import { useAuth } from '@clerk/clerk-react';
 import { useNavigate } from 'react-router-dom';
 
